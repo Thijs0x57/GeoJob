8273a09499da99e75cf4a077306f78eba82e4595
THIJS-PC <thijs.wijnen@gmail.com>
2017-12-05 20:03:22 +0100

    First Setup, GUI and activity's made

---
+++ app/src/main/java/nl/thijswijnen/geojob/WelcomeActivity.java
@@ -0,0 +1,17 @@
+package nl.thijswijnen.geojob;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+
+public class WelcomeActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+
+    }
+}
