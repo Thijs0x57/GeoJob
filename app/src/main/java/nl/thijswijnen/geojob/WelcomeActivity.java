a429b9e2f9dfa7a4b0448096e2aea592adefd465
THIJS-PC <thijs.wijnen@gmail.com>
2017-12-05 20:37:58 +0100

    edited some main settings

--- app/src/main/java/nl/thijswijnen/geojob/WelcomeActivity.java
+++ app/src/main/java/nl/thijswijnen/geojob/WelcomeActivity.java
@@ -3,6 +3,7 @@ package nl.thijswijnen.geojob;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Window;
+import android.widget.Button;
 
 public class WelcomeActivity extends AppCompatActivity
 {
