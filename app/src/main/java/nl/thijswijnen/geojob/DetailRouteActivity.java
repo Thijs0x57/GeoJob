e2cd401902cd6730405c07bc559e8737fd6055f8
THIJS-PC <thijs.wijnen@gmail.com>
2017-12-05 20:20:18 +0100

    added all activitys

---
+++ app/src/main/java/nl/thijswijnen/geojob/DetailRouteActivity.java
@@ -0,0 +1,15 @@
+package nl.thijswijnen.geojob;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DetailRouteActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_route);
+    }
+}
