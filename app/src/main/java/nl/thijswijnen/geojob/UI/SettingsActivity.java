a43c5b3b313ce19a6dfa74eea9d97612164ff39a
THIJS-PC <thijs.wijnen@gmail.com>
2017-12-11 09:40:24 +0100

    settings activity and buttons including navigation

--- app/src/main/java/nl/thijswijnen/geojob/UI/SettingsActivity.java
+++ app/src/main/java/nl/thijswijnen/geojob/UI/SettingsActivity.java
@@ -1,17 +1,33 @@
 package nl.thijswijnen.geojob.UI;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import nl.thijswijnen.geojob.R;
 
 public class SettingsActivity extends AppCompatActivity
 {
 
+    private Button taalKeuzeBtn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        taalKeuzeBtn = findViewById(R.id.settings_chooseLanguage_btn);
+        taalKeuzeBtn.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                Intent taalKeuzeIntent = new Intent(getApplicationContext(), LanguageActivity.class);
+                startActivity(taalKeuzeIntent);
+            }
+        });
     }
 }
