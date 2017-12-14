9fce58b9410e8715725898241baa17e14d2b03f0
THIJS-PC <thijs.wijnen@gmail.com>
2017-12-14 12:03:29 +0100

    some stuff

--- app/src/main/java/nl/thijswijnen/geojob/UI/DetailRouteActivity.java
+++ app/src/main/java/nl/thijswijnen/geojob/UI/DetailRouteActivity.java
@@ -1,7 +1,10 @@
 package nl.thijswijnen.geojob.UI;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import nl.thijswijnen.geojob.R;
 
@@ -13,5 +16,11 @@ public class DetailRouteActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail_route);
+
+        Button startRouteButton = findViewById(R.id.detailRoute_StartRoute_btn);
+        startRouteButton.setOnClickListener(view ->
+        {
+            startActivity(new Intent(getApplicationContext(), NavigateActivity.class));
+        });
     }
 }
