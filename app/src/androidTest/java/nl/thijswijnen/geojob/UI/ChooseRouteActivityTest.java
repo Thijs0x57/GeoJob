98ec8caca214ee0106ae811e6473169e0fa94e36
Unknown <maartenwn@gmail.com>
2017-12-14 15:53:32 +0100

    part 2

--- app/src/androidTest/java/nl/thijswijnen/geojob/UI/ChooseRouteActivityTest.java
+++ app/src/androidTest/java/nl/thijswijnen/geojob/UI/ChooseRouteActivityTest.java
@@ -1,8 +1,17 @@
+package nl.thijswijnen.geojob.UI;
+
+import org.junit.Before;
+
 import static org.junit.Assert.*;
 
 /**
  * Created by Maarten on 14/12/2017.
  */
 public class ChooseRouteActivityTest {
+
+
+    @Before
+    public void setUp() throws Exception {
+    }
 
 }
